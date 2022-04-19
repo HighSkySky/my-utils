9119e7df88c1279fdf723ff67c6d11d640ba4098
highskysky <highskysky97@gmail.com>
2022-04-19 10:30:03 +0800

    fix: 调整toAsync范型先后顺序

--- src/toAsync.ts
+++ src/toAsync.ts
@@ -2,7 +2,7 @@
  * 配合 await 以同步的模式捕获 Error，便于处理异常
  * @param { Promise } promise
  */
-export async function toAsync<T = any, E extends Error = Error>(
+export async function toAsync<E extends Error = Error, T = any>(
   promise: Promise<T>
 ): Promise<[T, undefined] | [undefined, E]> {
   try {
